2b9c77e7c2aba610b2a2a9d5254d0d78c5751e64
Andeo1812 <alexanforall@mail.ru>
2022-08-21 11:58:47 +0300

    task

--- intern_day/1/1.cpp
+++ intern_day/1/1.cpp
@@ -11,8 +11,6 @@ struct WeightedListGraph {
 
     WeightedListGraph();
 
-    void Resize();
-
     void AddEdge(int from, int to);
 
     ssize_t CheckEdge(int from, int to) const;
@@ -21,29 +19,50 @@ struct WeightedListGraph {
 
     size_t VerticesCount() const;
 
-    std::vector<std::pair<int, int>> GetNextVertices(int vertex) const;
+    std::vector <std::pair<int, int>> GetNextVertices(int vertex) const;
 
-    std::vector<std::pair<int, int>> GetPrevVertices(int vertex) const;
+    std::vector <std::pair<int, int>> GetPrevVertices(int vertex) const;
 
     WeightedListGraph(const std::vector <std::string> arr_str);
 
+    void WorkSubstr(std::string first, std::string next);
+
+    void Print() const;
+
+    void PrintTable() const;
+
+    void CreateTable(std::string word);
+
 private:
-    std::vector<std::vector<std::pair<int, int>>> adjacency_lists;
+    size_t count_edges;
+
+    std::vector <std::string> table;
+
+    std::vector <std::vector<std::pair < int, int>>>
+    adjacency_lists;
 };
 
-WeightedListGraph::WeightedListGraph() : adjacency_lists(8) {}
-
 void WeightedListGraph::AddEdge(int from, int to) {
+    //  std::cout << "add edge " << from << " " << to << " " << adjacency_lists[from].size() << std::endl;
+
+    //  std::cout << table[from] << " " << table[to] << " " << std::endl;
+
     adjacency_lists[from].push_back({1, to});
+    //  std::cout << "add edge " << adjacency_lists[from].size() << " " << adjacency_lists[from][adjacency_lists[from].size() - 1].second << std::endl;
+
+    ++count_edges;
 }
 
 ssize_t WeightedListGraph::CheckEdge(int from, int to) const {
-   for (size_t i = 0; i < adjacency_lists[from].size(); ++i) {
-       std::cout << adjacency_lists[from][i].second << " " << i << std::endl;
-       if (adjacency_lists[from][i].second == to) {
-           return i;
-       }
-   }
+    for (ssize_t i = 0; i < adjacency_lists[from].size(); ++i) {
+        //  std::cout << "check " << adjacency_lists[from][i].second << " " << i << " " << to << std::endl;
+
+        if (adjacency_lists[from][i].second == to) {
+            //  std::cout << "check " << adjacency_lists[from][i].second << " " << i << " " << to << std::endl;
+
+            return i;
+        }
+    }
 
     return -1;
 }
@@ -56,8 +75,8 @@ size_t WeightedListGraph::VerticesCount() const {
     return adjacency_lists.size();
 }
 
-std::vector<std::pair<int, int>> WeightedListGraph::GetNextVertices(int vertex) const {
-    std::vector<std::pair<int, int>> next_vertices(adjacency_lists[vertex].size());
+std::vector <std::pair<int, int>> WeightedListGraph::GetNextVertices(int vertex) const {
+    std::vector <std::pair<int, int>> next_vertices(adjacency_lists[vertex].size());
 
     for (size_t i = 0; i < adjacency_lists[vertex].size(); ++i) {
         next_vertices[i] = adjacency_lists[vertex][i];
@@ -66,11 +85,11 @@ std::vector<std::pair<int, int>> WeightedListGraph::GetNextVertices(int vertex) 
     return next_vertices;
 }
 
-std::vector<std::pair<int, int>> WeightedListGraph::GetPrevVertices(int vertex) const {
-    std::vector<std::pair<int, int>> prev_vertices;
+std::vector <std::pair<int, int>> WeightedListGraph::GetPrevVertices(int vertex) const {
+    std::vector <std::pair<int, int>> prev_vertices;
 
     for (size_t i = 0; i < adjacency_lists.size(); ++i) {
-        for (const auto &value : adjacency_lists[i]) {
+        for (const auto &value: adjacency_lists[i]) {
             if (vertex == value.second) {
 
                 prev_vertices.push_back(adjacency_lists[vertex][i]);
@@ -83,72 +102,110 @@ std::vector<std::pair<int, int>> WeightedListGraph::GetPrevVertices(int vertex) 
     return prev_vertices;
 }
 
+void WeightedListGraph::WorkSubstr(std::string first, std::string next) {
+    auto node_table_from = std::find(table.begin(), table.end(), first);
+
+    size_t index_from = std::distance(table.begin(), node_table_from);
+
+    auto node_table_to = std::find(table.begin(), table.end(), next);
+
+    size_t index_to = std::distance(table.begin(), node_table_to);
+
+    auto index_edge = CheckEdge(index_from, index_to);
+
+    if (index_edge == -1) {
+        AddEdge(index_from, index_to);
+    } else {
+        UpdateEdge(index_from, index_edge);
+    }
+
+}
+
+void WeightedListGraph::CreateTable(std::string word) {
+    if (!table.empty()) {
+        auto node_table = std::find(table.begin(), table.end(), word);
+
+        if (node_table == table.end()) {
+            table.push_back(word);
+
+            adjacency_lists.push_back({});
+
+        }
+    } else {
+        table.push_back(word);
+
+        adjacency_lists.push_back({});
+    }
+}
+
 
 WeightedListGraph::WeightedListGraph(const std::vector <std::string> arr_str) {
-    adjacency_lists.resize(10);
+    count_edges = 0;
 
-    std::vector<std::string> table;
-
-    for (const auto &str: arr_str) {
-        std::cout << str << std::endl;
-        for (size_t i = 0; i < str.size() - 3; ++i) {
-            std::string buf_first = str.substr(i, 3);
-
-            std::string buf_next = str.substr(i + 1, 3);
-
-            std::string buf;
-
-            if (!table.empty()) {
-                auto node_table_to = std::find(table.begin(), table.end(), buf_next);
-
-                if (node_table_to == table.end()) {
-                    table.push_back(buf_next);
-
-                    adjacency_lists[table.size() - 2].push_back({1, table.size() - 1});
-                } else {
-                    auto node_table_from = std::find(table.begin(), table.end(), buf_first);
-
-                    size_t index_from = std::distance(table.begin(), node_table_from);
-
-                    size_t index_to = std::distance(table.begin(), node_table_to);
-
-                    auto index_edge = CheckEdge(index_from, index_to);
-
-                    if (index_edge == -1) {
-                        AddEdge(index_from, index_to);
-                    } else {
-                        UpdateEdge(index_from, index_edge);
-                    }
-
-                }
-            } else {
-                table.push_back(buf_first);
-            }
-
+    for (size_t k = 0; k < arr_str.size(); ++k) {
+        for (size_t i = 0; i < arr_str[k].size() - 2; ++i) {
+            std::string word = arr_str[k].substr(i, 3);
+            CreateTable(word);
         }
     }
 
-    for (size_t i = 0; i < adjacency_lists.size(); ++i) {
-        for (size_t j = 0; j < adjacency_lists[i].size(); ++j) {
-            std::cout << table[i] << " " << table[j] << " " << adjacency_lists[i][j].first << std::endl;
+    //  PrintTable();
+
+    for (size_t k = 0; k < arr_str.size(); ++k) {
+        std::string buf_first;
+
+        std::string buf_next;
+
+        for (size_t i = 0; i < arr_str[k].size() - 3; ++i) {
+            buf_first = arr_str[k].substr(i, 3);
+
+            buf_next = arr_str[k].substr(i + 1, 3);
+
+            //  std::cout << buf_first << " " << buf_next << std::endl;
+            //  std::cout << i << std::endl;
+
+            WorkSubstr(buf_first, buf_next);
         }
+
+//        if (k != arr_str.size() - 1) {
+//            buf_first = arr_str[k].substr(0, 3);
+//
+//            WorkSubstr(buf_next, buf_next);
+//        }
     }
 
-    std::cout << std::endl;
+    Print();
+
+    //  PrintTable();
+}
+
+void WeightedListGraph::PrintTable() const {
+    std::cout << "-----------Table--------" << std::endl;
     for (auto n: table) {
         std::cout << n << std::endl;
     }
-
-    std::cout << adjacency_lists.size() << std::endl;
 }
 
+void WeightedListGraph::Print() const {
+    std::cout << adjacency_lists.size() << std::endl;
+
+    std::cout << count_edges << std::endl;
+
+    for (size_t i = 0; i < adjacency_lists.size(); ++i) {
+        //  std::cout << i << std::endl;
+        for (size_t j = 0; j < adjacency_lists[i].size(); ++j) {
+            std::cout << table[i] << " " << table[adjacency_lists[i][j].second] << " " << adjacency_lists[i][j].first
+                      << std::endl;
+        }
+    }
+}
 
 void run(std::istream &input, std::ostream &output) {
     size_t count = 0;
 
     input >> count;
 
-    std::vector<std::string> arr_str;
+    std::vector <std::string> arr_str;
 
     std::string buf;
 
